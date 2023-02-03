ca27b65bd4ccb0b0e777cb679d60e8ed588bb164
Luis <lmt@sanalto.com.mx>
2023-02-03 09:02:20 -0600

    Revert "Class created to handle upload of xls report files for due customers"
    This reverts commit e9fc23bb78bd589da6782ac9f3f7af053caebe7e.

--- clientes/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from clientes.models import Atrasado, Reporte 
-
-admin.site.register(Atrasado)
-admin.site.register(Reporte)
--- clientes/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Track
-
-class TrackForm(forms.ModelForm):
-    class Meta:
-        model = Track
-        fields = ['content', 'reporte']
