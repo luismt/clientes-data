6f81e88d2ce01529d78c77fa819518e054c37e94
Luis <lmt@sanalto.com.mx>
2023-02-10 10:12:21 -0600

    Merged

--- clientes/helpers.py
+++ clientes/helpers.py
@@ -1,101 +1,89 @@
-from os import getcwd, wait
-from .models import Atrasado, Reporte
-from openpyxl import load_workbook, Workbook
+import os
 from xls2xlsx import XLS2XLSX
+from openpyxl import load_workbook
 import pandas as pd
 from pandas._libs.tslibs.timestamps import Timestamp
-import numpy as np
 
-class Reportfile:
 
-    upload_authorized = False
+def convert_to_xlsx(file_name):
+    xls_dir = "/xls_files/"
+    xlsx_dir = "/xlsx_files/"
+    xls = XLS2XLSX(os.getcwd() + xls_dir + file_name)
+    xlsx_file = xls.to_xlsx(os.getcwd() + xlsx_dir + file_name + "x")
+    return xlsx_file
 
+def get_base_df(file_path):
+    wb = load_workbook(file_path)
+    ws = wb["Sheet1"]
+    base_df = pd.DataFrame(ws.values)
+    return base_df
+
+def get_pd_filt(df: pd.DataFrame, str_pattern: str):
+    return df.apply(lambda r: r.astype('string').str.contains(str_pattern).any(), axis=1)
+
+def get_reporte_name(df: pd.DataFrame):
+    options = ['Supendidos', 'Desconectados', 'corriente', 'Adelantados' ]
+    for option in options:
+        filt = get_pd_filt(df, option)
+        row_labels = df[filt]
+        if len(row_labels):
+            return option.lower()
+        
+def get_date(df: pd.DataFrame):
+    rows = len(df)
+    columns = len(df.columns)
+    for row in range(0, rows):
+        for column in range(0, columns):
+            if type(df.loc[row, column]) == Timestamp:
+                return df.loc[row, column]
+            
+def drop_columns(df: pd.DataFrame, columns):
+    df = df.drop(columns=columns)
+    return df
+
+def squezze_contrato(df: pd.DataFrame):
+    str_pattern = "Contrato"
+    filt = (df.apply(lambda r: r.astype("string").str.contains(str_pattern).any(), axis=1))
+    row_labels = df[filt]
+    c_range = row_labels.index[0]
+    c_range = int(float(c_range)) + 1
+    for i in range(0, c_range):
+        df.drop(i, inplace=True)
+    df.dropna(inplace=True)
+    return df
+
+
+class Reportefile:
+    reporte_name: str
+    df: pd.DataFrame
+    columns_to_drop = {
+        "corriente": [0, 2, 3, 4, 5, 6,7,9,10, 11, 12,13, 14, 15,16,18,19, 20, 21, 22],
+        "desconectados": [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 18, 19],
+        "supendidos": [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 18, 19],
+        "adelantados": [0, 2, 3, 4, 5, 6,7,9,10, 11, 12,13, 14, 15,16,18,19, 20, 21, 22],
+    }
+    
+    columns_to_rename = {
+        "corriente": ["contrato", "servicio", "periodo"],
+        "desconectados": ["contrato", "servicio", "periodo"],
+        "supendidos":["contrato", "servicio", "periodo"],
+        "adelantados": ["contrato", "servicio", "periodo"],
+    }
+    
     def __init__(self, filename):
         self.filename = filename
-        self.PWD = getcwd() 
-        self.check_db()
-
         
-    def check_db(self):
-        if self.filename in [reporte for reporte in Reporte.objects.all()]:
-            raise Exception("file alraedy in database")
-        else:
-            self.upload_authorized = True
-
     def to_sql(self):
-        xlsx = self.convert_to_xlsx()
-        base_df = self.get_base_df(xlsx)
-        timestamp = self.get_timestamp(base_df)
-        name = self.get_reporte_name(base_df)
-        self.clean_df(base_df)
-        reporte = Reporte(filename=self.filename, date=timestamp, report_type=name)
-        reporte.save()
-
+        self.xlsx_file = convert_to_xlsx(self.filename)
+        self.base_df = get_base_df(self.xlsx_file)
+        self.time = get_date(self.base_df)
+        self.reporte_name = get_reporte_name(self.base_df)
+        self.df = drop_columns(self.base_df, self.columns_to_drop.get(self.reporte_name))
+        self.df = squezze_contrato(self.df)
+        self.df.columns = self.columns_to_rename.get(self.reporte_name)
         self.create_entries(self.df, reporte)
-
-    def convert_to_xlsx(self):
-        xls_file = XLS2XLSX(self.PWD + "/xls_files/" + self.filename)
-        return xls_file.to_xlsx(self.PWD + "/xlsx_files/" + self.filename + "x")
-
-    def get_base_df(self, xlsx_path):
-        wb = load_workbook(xlsx_path)
-        ws = wb["Sheet1"]
-        base_df = pd.DataFrame(ws.values)
-        return base_df
-
-    def get_timestamp(self, base_df):
-        rows = len(base_df)
-        columns = len(base_df.columns)
-        for row in range(0, rows):
-            for column in range(0, columns):
-                if type(base_df.loc[row, column]) == Timestamp:
-                    return base_df.loc[row, column]
-
-    def get_reporte_name(self, base_df):
-        options = ['Supendidos', 'Desconectados',]
-        for option in options:
-            filt = get_pd_filt(base_df, option)
-            row_labels = base_df[filt]
-            if len(row_labels):
-                return option.lower()
-
-    def clean_df(self, base_df):
-        self.df = self.drop_columns_rows(base_df)
-        self.rename_columns()
-        return
-
-    def drop_columns_rows(self, base_df):
-        df = base_df.drop(columns=[0,2,4,5,7,9,10,12,13,16,19])
-        str_pattern = "Contrato" # search for patttern Contrato cell
-        
-        filt = (df
-                .apply(lambda r: r.astype('string').str.contains(str_pattern)
-                    .any(), axis=1))
-        row_labels =df [filt]
-        c_range = row_labels.index[0]
-        c_range = int(float(c_range)) + 1
-
-        for i in range(0, c_range):
-            df.drop(i, inplace=True)
-        df.dropna(inplace=True)
-        return df
-
-    def rename_columns(self):
-        column_names = ['contrato', 'nombre', 'mes', 'year', 'servicio', 'monto', 'celular', 'periodo', 'distribuidor']
-        self.df.columns = column_names
-        self.df.dropna(inplace=True)
-        self.df = self.df.astype({"year": 'int'})
-        return
 
     def create_entries(self, df, reporte):
         Atrasado.objects.bulk_create(
                 Atrasado(reporte=reporte, **vals) for vals in df.to_dict("records")
                 )
-
-    def check_authorized_upload(self):
-        if not self.upload_authorized:
-            raise Exception("Already in database")
-
-
-def get_pd_filt(df: pd.DataFrame, str_pattern: str):
-    return (df.apply(lambda r: r.astype('string').str.contains(str_pattern).any(), axis=1))
---
+++ clientes/management/commands/fullsolution.py
@@ -0,0 +1,33 @@
+from os import wait
+from django.core.management.base import BaseCommand
+import requests
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        url = "https://vivexux.fullsolutiondns.com/api/onu/get_all_onus_details"
+        headers={"X-Token": "2a251f2642474407a2d531ce54823de6"}
+        response = requests.get(url, headers=headers)
+        onus = response.json().get("onus")
+        filt = filter(filter_zone, onus)
+        maper = map(convert_onus, filt)
+        active_users = [m for m in maper]
+        print("Active users = %s", len(active_users))
+
+def filter_zone(onus):
+    if onus.get("zone_name") == "Quattrocom" or onus.get("name") == "ANGEL SANTIAGO UITZIL YAMA":
+        return False
+    else:
+        return True
+
+def convert_onus(onus):
+    converted = onus.get("name").split("-")[-2:]
+    # return convert
+    if len(converted[0]) == 4:
+        #print(onus.get("name"))
+        return converted[0] + "-" + converted[1]
+    else:
+        #print(onus.get("name"))
+        homoclave = converted[1]
+        cuenta = converted[0].split(" ")[-1:][0]
+        return cuenta + "-" + homoclave
